0461fe920b06c634a168b92b8ed5cb168b162f3b
yangzhengxing <1010621868@qq.com>
2018-11-01 20:03:28 +0800

    springboot整合jsp

---
+++ spring-boot-sample-web-jsp/src/main/java/sample/jsp/SpringBootSampleWebJspApplication.java
@@ -0,0 +1,12 @@
+package sample.jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleWebJspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSampleWebJspApplication.class, args);
+    }
+}
